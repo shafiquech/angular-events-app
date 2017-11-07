2b2860cb71a282b7df2755aab522d4b132a65c61
Jim Cooper <jim@cooperplanet.com>
2017-11-06 21:50:27 -0700

    Fix image path

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -17,7 +17,7 @@ export class EventsListComponent {
     date: '9/26/2036',
     time: '10:00 am',
     price: 599.99,
-    imageUrl: '/app/assets/images/angularconnect-shield.png',
+    imageUrl: '/assets/images/angularconnect-shield.png',
     location: {
       address: '1057 DT',
       city: 'London',
